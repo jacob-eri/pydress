b117e4913bf82d178435576304dc1a9eb926f1c4
jeriks <jacob.eriksson@physics.uu.se>
2022-11-21 10:11:45 +0000

    Added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dress',
+    version='1.0',
+    description='Directional Relativistic Spectrum Simulator',
+    author='Jacob Eriksson, Sean Conroy',
+    packages=find_packages(),
+)
