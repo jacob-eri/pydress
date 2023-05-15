09bc0cea2f7b561c2a04aa41d0d28749ffb3898d
jeriks <jacob.eriksson@physics.uu.se>
2023-05-15 10:14:36 +0100

    Added the tokamak module

---
+++ dress/tokamak/__init__.py
@@ -0,0 +1,6 @@
+"""The `tokamak` module is part of the `dress` package and contains 
+tools for performing reaction product spectra calculations in tokamak
+geometry, i.e. a toroidal system with toroidal symmetry, where quanteties
+are typically a function either of (R,Z) or of a flux surface label ρ."""
+
+import dress.tokamak.tokapart
