901203f82e45d46489c650887008110b1ee5724b
jeriks <jacob.eriksson@physics.uu.se>
2023-09-12 14:10:56 +0100

    Minor houskeeping

--- dress/config.py
+++ dress/config.py
@@ -1,5 +1,2 @@
-# Location of the dress source code folder
-# source_dir = '/home/jeriks/Utils/pydress'
-
 # Location of the cross section data
 cross_section_dir = '/home/jeriks/Utils/pydress/data/cross-sections'
