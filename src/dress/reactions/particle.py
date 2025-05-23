72ab7a74d32b51b710de687758898b21dd2b9529
Jacob (dell-lx) <jacob.o.eriksson@gmail.com>
2025-05-23 09:15:34 +0200

    Added alternate constructor for Particle class

--- src/dress/reactions/particle.py
+++ src/dress/reactions/particle.py
@@ -2,92 +2,109 @@ import numpy as np
 
 from dress.reactions import masses
 
+# Dictionary of particles known to DRESS and different name used to represent them.
 
+PARTICLE_DICT = {}
+
+PARTICLE_DICT['n'] = {'long name':'neutron',
+                      'Z':0, 'A':1, 'u':masses.mn,
+                      'name variations':('n', 'neutron'),
+                      'excitation energy':0.0}
+PARTICLE_DICT['p'] = {'long name':'proton',
+                      'Z':1, 'A':1, 'u':masses.mp,
+                      'name variations':('p', 'h', 'proton'),
+                      'excitation energy':0.0}
+PARTICLE_DICT['d'] = {'long name':'deuteron',
+                      'Z':1, 'A':2, 'u':masses.md,
+                      'name variations':('d', 'h2', '2h', 'deuteron'),
+                      'excitation energy':0.0}
+PARTICLE_DICT['t'] = {'long name':'triton',
+                      'Z':1, 'A':3, 'u':masses.mt,
+                      'name variations':('t', 'h3', '3h', 'triton'),
+                      'excitation energy':0.0}
+PARTICLE_DICT['he3'] = {'long name':'helium-3',
+                        'Z':2, 'A':3, 'u':masses.m3He,
+                        'name variations':('he3', '3he', 'helium-3'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['he4'] = {'long name':'helium-4',
+                        'Z':2, 'A':4, 'u':masses.m4He,
+                        'name variations':('he4', '4he', 'alpha', 'helium-4'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['li6'] = {'long name':'lithium-6',
+                        'Z':3, 'A':6, 'u':masses.m6Li,
+                        'name variations':('li6', '6li', 'lithium-6'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['be8'] = {'long name':'beryllium-8',
+                        'Z':4, 'A':8, 'u':masses.m8Be,
+                        'name variations':('be8', '8be', 'beryllium-8'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['be9'] = {'long name':'beryllium-9',
+                        'Z':4, 'A':8, 'u':masses.m9Be,
+                        'name variations':('be9', '9be', 'beryllium-9'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['b9'] = {'long name':'boron-9',
+                       'Z':5, 'A':9, 'u':masses.m9B,
+                       'name variations':('b9', '9b', 'boron-9')}
+PARTICLE_DICT['b10'] = {'long name':'boron-10',
+                        'Z':5, 'A':10, 'u':masses.m10B,
+                        'name variations':('b10', '10b', 'boron-10'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['c12'] = {'long name':'carbon-12',
+                        'Z':6, 'A':12, 'u':masses.m12C,
+                        'name variations':('c12', '12c', 'carbon-12'),
+                        'excitation energy':0.0}
+PARTICLE_DICT['c12(e1)'] = {'long name':'carbon-12 (1st excited state)',
+                            'Z':6, 'A':12, 'u':masses.m12C,
+                            'name variations':('12c(e1)', 'c12(e1)'),
+                            'excitation energy':4439.8}      # 1st excited state according to nndc.bnl.gov
+
+# Useful functions
+def _get_standard_names(input_name):
+    """Get the adopted short and long names for a given particle name or abbreviation."""
+
+    input_name = input_name.lower()
+    
+    for name, info in PARTICLE_DICT.items():
+        if input_name in info['name variations']:
+            # We have found the correct particle
+            standard_name = name 
+            standard_long_name = info['long name']
+            return standard_name, standard_long_name
+
+    raise ValueError(f'Name "{input_name}" does not represent a particle known to DRESS')
+    
+def get_name(A, Z):
+    """ Get particle name for given mass number A and atomic number Z."""
+    
+    for name, info in PARTICLE_DICT.items():
+        if (A == info['A']) and (Z == info['Z']):
+            return name
+
+    raise ValueError(f'A={A} and Z={Z} does not represent a particle known to DRESS')
+
+
+# Particle class
 class Particle:
     """A class for holding particle info."""
 
-    __slots__ = ('name', 'long_name', 'u', 'excitation_energy', 'm')
+    __slots__ = ('name', 'long_name', 'A', 'Z', 'u', 'excitation_energy', 'm')
 
     def __init__(self, name):
 
-        name = name.lower()
-
-        self.excitation_energy = 0.0
-
-        if name == 'n':
-            self.name = 'n'
-            self.long_name = 'neutron'
-            self.u = masses.mn
-
-        elif name in ['p', 'h']:
-            self.name = 'p'
-            self.long_name = 'proton'
-            self.u = masses.mp
-
-        elif name == 'd':
-            self.name = 'd'
-            self.long_name = 'deuteron'
-            self.u = masses.md
-
-        elif name == 't':
-            self.name = 't'
-            self.long_name = 'triton'
-            self.u = masses.mt
-
-        elif name in ['3he', 'he3']:
-            self.name = 'he3'
-            self.long_name = 'helium-3'
-            self.u = masses.m3He
-
-        elif name in ['4he', 'he4', 'alpha']:
-            self.name = 'he4'
-            self.long_name = 'helium-4'
-            self.u = masses.m4He
-
-        elif name in ['6li', 'li6']:
-            self.name = 'li6'
-            self.long_name = 'lithium-6'
-            self.u = masses.m6Li
-
-        elif name in ['8be', 'be8']:
-            self.name = 'be8'
-            self.long_name = 'beryllium-8'
-            self.u = masses.m8Be
-
-        elif name in ['9be', 'be9']:
-            self.name = 'be9'
-            self.long_name = 'beryllium-9'
-            self.u = masses.m9Be
-
-        elif name in ['9b', 'b9']:
-            self.name = 'b9'
-            self.long_name = 'boron-9'
-            self.u = masses.m9B
-
-        elif name in ['10b', 'b10']:
-            self.name = 'b10'
-            self.long_name = 'boron-10'
-            self.u = masses.m10B
-
-        elif name in ['12c', 'c12']:
-            self.name = 'c12'
-            self.long_name = 'carbon-12'
-            self.u = masses.m12C
-
-        elif name in ['12c(e1)', 'c12(e1)']:
-            self.name = 'c12(e1)'
-            self.long_name = 'carbon-12 (1st excited state)'
-            self.u = masses.m12C
-            
-            # 1st excited state according to nndc.bnl.gov
-            self.excitation_energy = 4439.8    # keV
-            
-        else:
-            raise ValueError(f'Invalid particle name: {name}')
+        self.name, self.long_name = _get_standard_names(name)
+        self.A, self.Z = PARTICLE_DICT[self.name]['A'], PARTICLE_DICT[self.name]['Z']
+        self.u = PARTICLE_DICT[self.name]['u']
+        self.excitation_energy = PARTICLE_DICT[self.name]['excitation energy']
 
         self.m = self.u * masses.u_keV + self.excitation_energy
 
 
+    @classmethod
+    def from_AZ(cls, A, Z):
+        name = get_name(A, Z)
+        return cls(name)
+    
+        
     def __repr__(self):
         return 'Particle: {}'.format(self.long_name)
 
